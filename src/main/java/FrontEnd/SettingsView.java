251f4383681f02030f7f1418073517602b163eb2
Kaffite <karlmarkushannust@gmail.com>
2025-08-24 15:20:31 +0300

    Better Settings menu

--- src/main/java/FrontEnd/SettingsView.java
+++ src/main/java/FrontEnd/SettingsView.java
@@ -11,6 +11,8 @@ import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 // View for quiz settings
 public class SettingsView {
@@ -19,36 +21,38 @@ public class SettingsView {
         BorderPane BPane = new BorderPane();
         Scene scene = View.baseScene(BPane);
 
+        VBox verticalBox = new VBox(120, new Text()); // VBox for text and Button
+        verticalBox.setAlignment(Pos.TOP_CENTER);
+        BPane.setCenter(verticalBox);
+
         // Text
-        VBox verticalBox = new VBox();
-        verticalBox.setAlignment(Pos.CENTER);
-        String [] messages = {
-                "", "",
+        String[] arrayMessages = {
                 "Miks sa siin oled?",
                 "Mis seaded sellel quiz-il olema peaks?",
                 "Ära tegele lollustega, mine tee parem see quiz ära."};
-        for (int i = 0; i < messages.length; i++){
-            Text text = new Text(messages[i].toString());
-            text.getStyleClass().add("h1");
-            verticalBox.getChildren().add(text);
-        }
+        // Arraylist of messages that are displayed after going to SettingsView:
+        ArrayList<String> messages = new ArrayList<>(Arrays.asList(arrayMessages));
+        Text text = new Text(messages.removeFirst());
+        text.getStyleClass().add("h1");
+        verticalBox.getChildren().add(text);
 
-        // Return button
-        Button returnBtn = new Button("Tagasi menüüsse");
+        // Button
+        Button returnBtn = new Button("Edasi");
         returnBtn.getStyleClass().addAll("btn");
         returnBtn.setOnMouseClicked(mouseEvent -> {
-            try {
-                Main.showView(stage, MenuView.getScene(stage), "Milline informaatik sa oled?");
-            } catch (IOException e) {
-                throw new RuntimeException(e);
+            // If there are no more messages to display, return to menu
+            if (messages.isEmpty() ){
+                try {
+                    Main.showView(stage, MenuView.getScene(stage), "Milline informaatik sa oled?");
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            } else { // Display messages from Arraylist 'messages'
+                if (messages.size() == 1) returnBtn.setText("Tagasi menüüsse");
+                text.setText(messages.removeFirst());
             }
         });
-        VBox btnBox = new VBox(returnBtn);
-        btnBox.setAlignment(Pos.CENTER);
-        BPane.setCenter(btnBox);
-
-        verticalBox.setSpacing(15);
-        BPane.setTop(verticalBox);
+        verticalBox.getChildren().add(returnBtn);
 
         return scene;
     }
